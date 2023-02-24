fc320c34144b81c4c0524509650cdeb735ddbc6b
Svetoslav Domanov <svetoslav.domanov@dreamix.eu>
2023-02-24 12:35:35 +0000

    Added User registration

---
+++ src/hooks/auth.js
@@ -0,0 +1,7 @@
+import { useMutation } from "@tanstack/react-query";
+import { AuthApi } from "../api";
+
+export function useRegister({ onSuccess }) {
+  const { mutate, isLoading, error } = useMutation({ mutationFn: AuthApi.register, onSuccess });
+  return { register: mutate, isLoading, error };
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import App from "./App";
 
 import { prepareMocks } from "./mocks";
-
 import "./index.css";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 prepareMocks().then(() => {
-  ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode>Hello World!</React.StrictMode>);
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </React.StrictMode>
+  );
 });
