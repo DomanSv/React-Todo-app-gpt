e88d3108a445f59f00c6cfcae1f33e7e7b6dd0ab
Svetoslav Domanov <Svetoslav.Domanov@aerlingus.com>
2023-03-13 17:56:01 +0200

    Use tailwind for input style

--- src/components/forms/Input.jsx
+++ src/components/forms/Input.jsx
@@ -3,21 +3,12 @@ import { forwardRef } from "react";
 function Input(props, ref) {
   const { error, name, id = name, label, ...rest } = props;
   const inputError = error?.[name]?.message;
-
-  const inputStyle = {
-    backgroundColor: "#f2f2f2",
-    border: "none",
-    width: "300px",
-    padding: "10px",
-    borderRadius: "5px",
-    fontSize: "16px",
-  };
-
+  
   return (
     <div>
       <label htmlFor={id} className='flex flex-col gap-2'>
         {label && <span>{label}</span>}
-        <input name={name} id={id} {...rest} ref={ref} style={inputStyle} />
+        <input className='border-none rounded-md' name={name} id={id} {...rest} ref={ref} />
         {inputError && <span className='text-red-500'>{inputError}</span>}
       </label>
     </div>
